8c4ccae5783f60afae267ed20e63454fe92ae325
Dev-ZTQGames <dev@ztqgames.com>
2024-08-01 15:41:38 +0000

    v0.1.5

---
+++ canister.sh
@@ -0,0 +1,12 @@
+dfx deploy --playground NFT_MiningMaze --argument "(
+  principal \"p63kj-vlrqf-chucp-shmgr-gnuj7-uop7f-flj32-qzfgc-l55nf-vuutz-gae\", 
+  record {
+    logo = record {
+      logo_type = \"image/png\";
+      data = \"\";
+    };
+    name = \"Mining Maze\";
+    symbol = \"MM\";
+    maxLimit = 9554;
+  }
+)"

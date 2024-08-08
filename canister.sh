aba52da42cd0074d606de901ae719cc9aa2367b5
Dev-ZTQGames <dev@ztqgames.com>
2024-08-08 09:19:16 +0000

    v0.1.8

--- canister.sh
+++ canister.sh
@@ -1,4 +1,4 @@
-dfx deploy --playground NFT_MiningMaze --argument "(
+dfx deploy --network=ic NFT_MiningMaze_test --argument "(
   principal \"p63kj-vlrqf-chucp-shmgr-gnuj7-uop7f-flj32-qzfgc-l55nf-vuutz-gae\", 
   record {
     logo = record {
